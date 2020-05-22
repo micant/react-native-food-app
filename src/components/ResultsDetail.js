a0757c42c325ed68625ee518d89a9df8f43dffed
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-22 14:38:15 +1000

    ResultsDetail styles

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
+import { AuthSession } from 'expo';
 
 const ResultsDetail = ({ result }) => {
     return <View>
-        <Text>{ result.name }</Text> 
+        <Image style={styles.image} source={{ uri: result.image_url }} />
+        <Text style={styles.name}>{result.name}</Text>
+        <Text>{result.rating} Stars, {result.review_count} reviews</Text>
     </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+});
 
 export default ResultsDetail;
 
